23f37462266fd7f2b050f30750566ce8ac02b89e
Firstname Lastname <jagcoronado@gmail.com>
2015-10-25 11:04:26 +0100

    first commit

---
+++ cookiechoices-site.js
@@ -0,0 +1,10 @@
+var demoButterBar = function() {
+    cookieChoices.showCookieConsentBar('Your message for visitors here.',
+        'close message', 'learn more', 'http://example.com');
+};
+
+var demoSplashScreen = function() {
+cookieChoices.showCookieConsentDialog('Your message for visitors here.',
+    'close message', 'learn more', 'http://example.com');
+};
+
