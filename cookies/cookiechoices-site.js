bb77fe5f1f5a7d44a2ae38146756c5e0e68ea496
Firstname Lastname <jagcoronado@gmail.com>
2015-10-25 11:14:46 +0100

    Inserting sample fro logger

--- cookies/cookiechoices-site.js
+++ cookies/cookiechoices-site.js
@@ -8,3 +8,11 @@ cookieChoices.showCookieConsentDialog('Your message for visitors here.',
     'close message', 'learn more', 'http://example.com');
 };
 
+document.addEventListener('DOMContentLoaded', function(event) {
+window.cookieChoices && cookieChoices.showCookieConsentBar && cookieChoices.showCookieConsentBar(
+    (window.cookieOptions && cookieOptions.msg) || 'Este sitio emplea cookies de Google para prestar sus servicios, para personalizar anuncios y para analizar el tráfico. Google recibe información sobre tu uso de este sitio web. Si utilizas este sitio web, se sobreentiende que aceptas el uso de cookies.',
+    (window.cookieOptions && cookieOptions.close) || 'Entendido',
+    (window.cookieOptions && cookieOptions.learn) || 'Más información',
+    (window.cookieOptions && cookieOptions.link) || 'https://www.blogger.com/go/blogspot-cookies');
+});
+
