703425c4bfbf9a0fe2bd208d2c836fff72fdbf78
Software factory <factory@coronadoland.com>
2024-12-04 19:00:58 +0100

    install

--- cf.sh
+++ cf.sh
@@ -9,13 +9,12 @@
 # Date: 2024-12-04
 #
 
-git="https://raw.githubusercontent.com/coronadofactory/site-builder/refs/heads/main"
+git="https://raw.githubusercontent.com/coronadofactory/site-builder/refs/heads/master"
 src="src"
 infrastructure="src/infrastructure"
 public="public"
 
 module=$1
-
 
 if [ ! -d "node_modules" ]; then
 
@@ -26,4 +25,15 @@ fi
 
 if [ ! -d "$infrastructure" ]; then
   mkdir $infrastructure
+fi
+
+
+re="(cookies)"
+if [[ $module =~ $re ]]; then
+      curl $git/cookies/$module-manager.js -o $infrastructure/$module-manager.js
+fi
+
+re="(analytics|youtube)"
+if [[ $module =~ $re ]]; then
+      curl $git/cookies/$module.js -o $infrastructure/$module.js
 fi
